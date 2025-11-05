3d5688c9dcd50ab2a45f3fb778adefcd00bff7b5
HannaPolishchuk <hanna.polishchuk@gmail.com>
2025-11-06 00:11:05 +0100

    Arreglé setTimeout y la documentación.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const usuario = {
     email: "fulanito123@gmail.com",
 }
 
-//======================PROMESAS===============
+//======================PROMESAS==================
 
 /**
  * Agrega un Listener al botón para obtener la respuesta del usuario.
@@ -19,18 +19,20 @@ btn.addEventListener('click', () => {
 });
 
 /**
- * Función para renderizar los datos sobre el usuario.
- * @param {Object} usuario Objeto que contiene los datos del usuario
- * @returns {Object} Objeto que contiene los datos del usuario
+ * Función para renderizar los datos sobre el usuario en el DOM.
+ * @param {Object} usuario - Objeto que contiene los datos del usuario.
+ * @param {string} usuario.nombre - Nombre del usuario.
+ * @param {string} usuario.email - Correo electrónico del usuario.
+ * @returns {Object} usuario - Objeto que contiene los datos del usuario
  */
-const renderizarDatos = (usuario) => {
+const renderizarDatos = ({ nombre, email }) => {
     const userContainer = document.createElement("DIV");
     const pNombre = document.createElement("P");
     const pCorreo = document.createElement("P");
 
     userContainer.classList.add('userContainer');
-    pNombre.textContent = usuario.nombre;
-    pCorreo.textContent = usuario.email;
+    pNombre.textContent = nombre;
+    pCorreo.textContent = email;
 
     userContainer.append(pNombre);
     userContainer.append(pCorreo);
@@ -39,28 +41,29 @@ const renderizarDatos = (usuario) => {
     return usuario;
 }
 /**
- * Función que simule una llamada a una API y devuelva una promesa.
- * La promesa se resuelve después de un retraso simulado (2s).
- * @returns {Promise} Promesa que se resuelve con los datos del usario
+ * Función que simula una llamada a una API y devuelve una promesa.
+ * La promesa se resuelve después de un retraso simulado (2 segundos).
+ * @returns {Promise<Object>} Promesa que se resuelve con el objeto del usario
  */
 const llamarAPI = () => {
     return new Promise((resolve, reject) => {
         //la variable temporal para testear las funciones:
-        let chivato = true;
-        if (chivato === true) {
-            setTimeout(() => {
+        setTimeout(() => {
+            let chivato = true;
+            if (chivato === true) {
                 resolve(usuario);
-            }, 2000);
-        } else {
-            setTimeout(() => {
+            } else {
                 reject('No se sabe. Sigue buscando...');
-            }, 2000);
-        }
+            }
+        }, 2000);
     });
 }
 
 /**
- * Invocación de la promesa.
+ * Invoca la llamada simulada a la API y renderiza los datos del usuario.
+ * Maneja la promesa utilizando .then() y .catch.
+ * @function recibirRespuesta
+ * @returns {void} No devuelve ningún valor directamente.
  */
 // const recibirRespuesta = () => {
 //     llamarAPI()
@@ -72,17 +75,20 @@ const llamarAPI = () => {
 //         })
 // }
 
-//========================AWAIT===================
+//========================AWAIT======================
 /**
- * Invocación de la promesa.
+ *  * Invoca la llamada simulada a la API y renderiza los datos del usuario.
  * @async
- * @returns {Promise}
+ * @returns {Promise<Object>} Promesa que se resuelve con el objeto del usario
+ * @throws {Error|string} Si ocurre un error durante la llamada a la API.
  */
 const recibirRespuesta = async () => {
     try {
         let respuesta = await llamarAPI();
         renderizarDatos(respuesta);
+        return (respuesta)
     } catch (error) {
         infoUsuario.textContent = error;
+        throw error;
     }
 }
